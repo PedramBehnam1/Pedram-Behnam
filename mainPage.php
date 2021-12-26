a9fd6822e5a8e31145109482025362887442e026
PedramBehnam1 <pedrambehnam4586@gmail.com>
2021-12-26 15:33:51 +0330

    modified and add files

--- mainPage.php
+++ mainPage.php
@@ -1,14 +1,44 @@
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>table</title>
-    </head>
-    <body>
-        <h1 align="center">My table</h1>
-        <?php
-            include ("table.php");
-        ?>
-    </body>
+<!DOCTYPE html>
+ <html>
+ <head>
+		<title>calculator</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="icon" href="icons/calculator.ico">
+	</head>
+
+
+ <body>
+   <div>
+				<h1>Calculator</h1>
+ <?php
+		
+   include ('objects/Calculator.php');
+			use objects\makeCalculator as cal;
+
+		$Calculator1 = new cal();
+
+		$Calculator1->calculator();
+		echo "<p>the answer is : </p>";
+		
+		if(isset($_GET['submit'])){
+				$result = $_GET['Expression'];
+				$result =	str_replace(' ', '', $result);
+    
+				if(preg_match('~[\^\+\-\/\*]$~',$result)){
+					echo "please enter Mathmatical Expression";
+				} else if(preg_match('~^[0-9()\+\-*\/]+$~', $result)){
+					eval("echo $result;");
+				
+				}else if(preg_match('~^[0-9()\+\-*\^\/\%]+$~', $result)){
+				$result = str_replace('^', '**', $result);
+				eval("echo $result;");
+					
+			}else
+				echo "please enter Mathmatical Expression"; 
+		}
+
+ ?>
+		<a href="MainPage.php"> back link </a>
+		</div>
+	</body>
 </html>
