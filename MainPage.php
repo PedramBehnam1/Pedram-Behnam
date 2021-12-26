d20999cc00dd2dd251eead5f3c251d9e6b3954e1
PedramBehnam1 <pedrambehnam4586@gmail.com>
2021-12-26 15:04:26 +0330

    modified and add files

---
+++ MainPage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+		<title>calculator</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="icon" href="icons/calculator.ico">
+	</head>
+
+
+ <body>
+   <div>
+				<h1>Calculator</h1>
+ <?php
+		
+   include ('objects/Calculator.php');
+			use objects\makeCalculator as cal;
+
+		$Calculator1 = new cal();
+
+		$Calculator1->calculator();
+		echo "<p>the answer is : </p>";
+		
+		if(isset($_GET['submit'])){
+				$result = $_GET['Expression'];
+				$result =	str_replace(' ', '', $result);
+				if(preg_match('~[\^\+\-\/\*]$~',$result)){
+					echo "please enter Mathmatical Expression";
+				} else if(preg_match('~^[0-9()\+\-*\/]+$~', $result)){
+					eval("echo $result;");
+				
+				}else if(preg_match('~^[0-9()\+\-*\^\/\%]+$~', $result)){
+				$result = str_replace('^', '**', $result);
+				eval("echo $result;");
+					
+			}else
+				echo "please enter Mathmatical Expression"; 
+		}
+
+ ?>
+		<a href="MainPage.php"> back link </a>
+		</div>
+	</body>
+</html>
---
+++ objects/Calculator.php
@@ -0,0 +1,21 @@
+<?php
+  namespace objects; 
+  
+ 
+
+ class makeCalculator
+ {
+
+  public function calculator(){
+    echo "<form>";
+      echo "<input type=text id=Expression name=Expression placeholder=Mathmatical_Expression >";
+      echo "<button type=submit id=submit name=submit value=submit>Calculate</button>";
+    echo "</form>";
+  }
+
+ 
+
+ }
+ 
+?>
+
